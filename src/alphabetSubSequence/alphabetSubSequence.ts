3983fbed8c7f2dcf1519045a912e639cc3ae60ea
Luciano de Souza Nery <lsnery@gmail.com>
2019-09-27 19:51:50 -0300

    added funcional approach

--- src/alphabetSubSequence/alphabetSubSequence.ts
+++ src/alphabetSubSequence/alphabetSubSequence.ts
@@ -1,3 +1,5 @@
+import { log } from 'util';
+
 const alphabetic = 'abcdefghijklmnopqrstuvwxyz';
 
 /*
@@ -37,8 +39,8 @@ const subSequenceInstructor = (s: string): boolean => {
 		return false;
 	}
 
-	for (let i = 0; i < charValues.length - 1; i++){
-		if (charValues[i] >= charValues[i + 1]){
+	for (let i = 0; i < charValues.length - 1; i++) {
+		if (charValues[i] >= charValues[i + 1]) {
 			return false;
 		}
 	}
@@ -50,3 +52,23 @@ console.log(subSequenceInstructor('effg'));
 console.log(subSequenceInstructor('cdce'));
 console.log(subSequenceInstructor('ace'));
 console.log(subSequenceInstructor('bxz'));
+
+/*
+ * Option 3 - Functional Approach
+ */
+
+const subSequenceFunctional = (sequence: string): boolean => {
+	let isValid = true;
+
+	const chars = sequence.split('').map(x => x.charCodeAt(0)) /*?*/;
+	chars.forEach((value, i) => {
+		if (value >= chars[i + 1]) isValid = false;
+	});
+
+	return isValid;
+};
+
+console.log(subSequenceFunctional('effg'));
+console.log(subSequenceFunctional('cdce'));
+console.log(subSequenceFunctional('ace'));
+console.log(subSequenceFunctional('bxz'));
