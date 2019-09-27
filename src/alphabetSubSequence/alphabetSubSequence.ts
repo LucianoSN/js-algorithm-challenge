de8605267f1f324e95aaef37d66ee56298aedafa
Luciano de Souza Nery <lsnery@gmail.com>
2019-09-27 15:04:06 -0300

    instructor solution

--- src/alphabetSubSequence/alphabetSubSequence.ts
+++ src/alphabetSubSequence/alphabetSubSequence.ts
@@ -10,7 +10,7 @@ const subSequenceMy = (sequence: string): boolean => {
 	const index = sequence.split('').map(x => alphabetic.indexOf(x)); /*?*/
 
 	index.forEach((x, i) => {
-		if (index[i + 1] <= x) isValid = false;
+		if (x >= index[i + 1]) isValid = false;
 	});
 
 	return isValid;
@@ -20,3 +20,33 @@ console.log(subSequenceMy('effg'));
 console.log(subSequenceMy('cdce'));
 console.log(subSequenceMy('ace'));
 console.log(subSequenceMy('bxz'));
+
+/*
+ * Option 2 - Instructor Approach
+ */
+
+const subSequenceInstructor = (s: string): boolean => {
+	const chars: string[] = s.split(''); /*?*/
+	const charValues: number[] = [];
+
+	chars.forEach((char: string) => {
+		charValues.push(char.charCodeAt(0));
+	});
+
+	if (new Set(charValues).size !== charValues.length) {
+		return false;
+	}
+
+	for (let i = 0; i < charValues.length - 1; i++){
+		if (charValues[i] >= charValues[i + 1]){
+			return false;
+		}
+	}
+
+	return true;
+};
+
+console.log(subSequenceInstructor('effg'));
+console.log(subSequenceInstructor('cdce'));
+console.log(subSequenceInstructor('ace'));
+console.log(subSequenceInstructor('bxz'));
