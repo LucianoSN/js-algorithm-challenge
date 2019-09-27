d9de780d6ca02c42547fb14acc6c323d4ee6e494
Luciano de Souza Nery <lsnery@gmail.com>
2019-09-27 14:40:55 -0300

    alphabetic subsequence

--- src/alphabetSubSequence/alphabetSubSequence.ts
+++ src/alphabetSubSequence/alphabetSubSequence.ts
@@ -1,3 +1,22 @@
+const alphabetic = 'abcdefghijklmnopqrstuvwxyz';
+
 /*
  * Option 1 - My First Solution
  */
+
+const subSequenceMy = (sequence: string): boolean => {
+	let isValid = true;
+
+	const index = sequence.split('').map(x => alphabetic.indexOf(x)); /*?*/
+
+	index.forEach((x, i) => {
+		if (index[i + 1] <= x) isValid = false;
+	});
+
+	return isValid;
+};
+
+console.log(subSequenceMy('effg'));
+console.log(subSequenceMy('cdce'));
+console.log(subSequenceMy('ace'));
+console.log(subSequenceMy('bxz'));
