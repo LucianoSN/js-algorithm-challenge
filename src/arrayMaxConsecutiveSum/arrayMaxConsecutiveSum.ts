179b6132f0db1de568c7355d6a67932691554b5b
Luciano de Souza Nery <lsnery@gmail.com>
2019-10-03 14:26:47 -0300

    array max consecutive  sum

--- src/arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
+++ src/arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
@@ -16,3 +16,40 @@ const arrayMaxMy = (input: number[], k: number): number => {
 };
 // There is an error, the K variable is not used
 console.log(arrayMaxMy([2, 3, 5, 1, 6], 2));
+
+/*
+ * Option 2 - Instructor Solution
+ */
+
+const arrayMaxInstructor = (input: number[], k: number): number => {
+	let sum = 0;
+
+	for (let i = 0; i < k; i++) {
+		sum += input[i]; /*?*/
+	}
+
+	let max = sum; /*?*/
+
+	for (let i = k; i < input.length; i++) {
+		sum -= input[i - k]; /*?*/
+		sum += input[i]; /*?*/
+
+		console.log(sum, max);
+
+		if (sum > max) {
+			max = sum; /*?*/
+		}
+	}
+
+	return max;
+};
+
+console.log(arrayMaxInstructor([2, 3, 5, 1, 6], 2));
+
+/*
+ * Option 3 - Functional Approach
+ */
+
+const arrayMaxFunctional = (input: number[], k: number): number => {};
+
+console.log(arrayMaxFunctional([2, 3, 5, 1, 6], 2));
